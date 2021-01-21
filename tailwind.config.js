5189c6552b49b1c680f75eed9989d2b633235694
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2021-01-21 18:01:45 +0800

    [FEATURE]: root: added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './pages/**/*.tsx',
+    './src/component/**/*.tsx',
+    './src/module/**/*.tsx'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
