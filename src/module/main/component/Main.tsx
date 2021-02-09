a6803731c048ae4dcb75802536000d91062665f8
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2021-02-09 10:45:26 +0800

    [REFACTOR]: next-recoil: remove tailwindcss and related libs

--- src/module/main/component/Main.tsx
+++ src/module/main/component/Main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import Recoil from 'recoil';
-import 'tailwindcss/tailwind.css';
 
 export const App = ({ Component, pageProps }: AppProps) => {
     return (
